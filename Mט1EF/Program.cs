ebe8391257967cab7ff15f4423314e2b5464760d
ErezHackeru <45459156+ErezHackeru@users.noreply.github.com>
2019-05-04 23:48:10 +0300

    Add files via upload

---
+++ Mט1EF/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mט1EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ReservationManagerDBEntities ResEntities = new ReservationManagerDBEntities())
+            {
+                var clients = (from c in ResEntities.Clients
+                               select c).ToList();
+
+                clients.ForEach(e => Console.WriteLine($"id {e.ID}, "+
+                    $"Password {e.Password} , PrivateName {e.PrivateName}, +" +
+                    $"SurName {e.SurName} , UserName {e.UserName} " +
+                    $", CreditCardNumber {e.CreditCardNumber}"));
+
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
